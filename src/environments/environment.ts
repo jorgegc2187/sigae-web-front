2fabe4010593c4f51b61ea9e77ccee2a0a45ad9d
Jorge Gonzales <jorgegc2187@gmail.com>
2026-07-28 23:12:23 -0500

    feat(deploy): configure Railway API endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
+  apiUrl: 'https://sigae-web-api-dev.up.railway.app/api',
   appName: 'Sistema de Gestión de Activos',
 } as const;
